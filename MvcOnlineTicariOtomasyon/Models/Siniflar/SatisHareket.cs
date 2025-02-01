f51ffa4cdd76cb7ed21dc34135c1ad85ff98a0d7
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-02-01 23:33:45 +0300

    First commit

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHareket
+    {
+        [Key]
+        public int SatisId { get; set; }
+        //ürün tablosu ile ilişkilendirme   
+        //cari
+        //personel
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public Decimal Fiyat { get; set; }
+        public Decimal ToplamTutar { get; set; }
+        public ICollection<Urun> Uruns { get; set; }
+        public ICollection<Cariler> Carilers { get; set; }
+        public ICollection<Personel> Personels { get; set; }
+    }
+}
